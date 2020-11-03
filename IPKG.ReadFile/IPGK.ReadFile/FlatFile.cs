123c397db1c9a2a36bfec35de9d152e307b17c25
Joris Ribbens <joris@jorisr.be>
2020-11-03 13:24:18 +0100

    1

---
+++ IPKG.ReadFile/IPGK.ReadFile/FlatFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IPGK.ReadFile
+{
+    public class FlatFile
+    {
+        public string Read(string file)
+        {
+            var lines = System.IO.File.ReadAllLines(file);
+            return string.Join(",", lines);
+        }
+    }
+}
---
+++ IPKG.ReadFile/IPGK.ReadFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IPGK.ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give the file to read");
+            var fileName = Console.ReadLine();
+            var file = new FlatFile();
+            Console.Write(file.Read(fileName));
+
+        }
+    }
+}
